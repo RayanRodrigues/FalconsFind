6ed2b268afb2a7ae076ab3512964a5f52947551f
hendricknkuba <imhendrick16@gmail.com>
2026-03-18 14:18:37 -0700

    feat: add student authentication guard to manage access based on user role

---
+++ frontend/src/app/core/guards/student-auth.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import type { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { UserRole } from '../../models';
+import { AuthService } from '../services/auth.service';
+
+export const studentAuthGuard: CanActivateFn = (route) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const session = authService.getStoredSession();
+
+  if (!session) {
+    const returnUrl = route.url.map(s => s.path).join('/');
+    return router.parseUrl(`/register?returnUrl=/${returnUrl}`);
+  }
+
+  if (session.user.role !== UserRole.STUDENT) {
+    authService.logoutStudent();
+    return router.parseUrl('/register');
+  }
+
+  return true;
+};
